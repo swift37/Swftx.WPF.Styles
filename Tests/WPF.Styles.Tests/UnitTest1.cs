cf0ac5b760f1cbda84cc0a90ba132c17c4cadbef
Swift <rafchik37@gmail.com>
2023-04-13 16:36:55 +0200

    Добавление модульного тестирования (пустой тест для настройки автоматизации)

---
+++ Tests/WPF.Styles.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WPF.Styles.Tests
+{
+    [TestClass]
+    public class UnitTestFirst
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
